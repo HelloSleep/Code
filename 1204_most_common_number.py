843ca026e5a88b721a7a2530958a7b0271d6af7b
HelloSleep <nargene@gmail.com>
2022-07-13 23:21:30 +0900

    Success problem was range

--- 1204_most_common_number.py
+++ 1204_most_common_number.py
@@ -5,11 +5,12 @@ sys.stdin = open("input.txt", "r")
 
 T= int(input())
 
-for test_case in (1,T+1):
+for test_case in range(1,T+1):
     case= int(input())
     price =list(map(int, input().split()))
     cprice = Counter(price)
     print(f"#{test_case} {cprice.most_common(1)[0][0]}")
+    
     
 
 
